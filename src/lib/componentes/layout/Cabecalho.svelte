8438be420598e881b5d8bd80d299c5b0c386ffbd
Mauricio Oliveira <ciumauri@gmail.com>
2023-01-24 17:34:03 -0300

    refatorando o código

---
+++ src/lib/componentes/layout/Cabecalho.svelte
@@ -0,0 +1,29 @@
+<header>
+	<img
+		src="
+    /imagens/d2p_logo.png"
+		alt="Logo da D2P"
+		class="logo"
+	/>
+</header>
+
+<style>
+	header {
+		--largura-tela-base: 1440;
+
+		background: url('/imagens/header.png') no-repeat;
+		background-size: 100%;
+
+		height: calc((225 / var(--largura-tela-base)) * 100vw);
+		margin-bottom: 50rem;
+
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+	.logo {
+		width: 100%;
+		max-width: 150px;
+		align-self: center;
+	}
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import Cabecalho from '$lib/componentes/layout/Cabecalho.svelte';
+	import Rodape from '$lib/componentes/layout/Rodape.svelte';
+	import '../app.css';
+</script>
+
+<div class="container-principal">
+	<Cabecalho />
+	<slot><!-- optional fallback --></slot>
+	<Rodape />
+</div>
+
+<style>
+	.container-principal {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+
+	/* .estilo-principal {
+		text-align: center;
+		padding: 0 5vw 3.375rem;
+		flex: 1;
+	}
+
+	.minha-lista-container {
+		margin-bottom: 2rem;
+	}
+
+	.divisoria {
+		width: 40vw;
+		height: 2px;
+		background-color: var(--verde);
+		margin: 0 auto;
+	} */
+</style>
